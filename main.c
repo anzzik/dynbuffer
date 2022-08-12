1b733c53f563417e97c766dad64ea07064c053f5
Anssi Kulju <anssi.kulju@gmail.com>
2022-08-12 09:33:28 +0300

    main.c cleaned up.

--- main.c
+++ main.c
@@ -5,44 +5,32 @@
 
 int main(void)
 {
-	Buffer_t *b = buffer_create_from_string("DDDDCCCCBBBBAAAA");
-	/* buffer_load_from_string(b ,"AAABBBCCC"); */
+	Buffer_t *b = buffer_create_empty();
 
-	char *str = buffer_write_to_string(b);
-	printf("output str: %s\n", str);
-	return 0;
+	buffer_load_from_string(b ,"AAABBBCCC");
 
+	buffer_append_char(b, 'f');
+	buffer_append_char(b, 'f');
+	buffer_append_char(b, 'f');
+	buffer_append_char(b, 'f');
+	buffer_append_char(b, 'f');
+	buffer_append_char(b, 'f');
 	buffer_print(b);
-	buffer_free(b);
-	return 0;
 
 	buffer_load_from_file(b, "testfile.txt");
 	buffer_write_to_file(b, "testfile_wr.txt");
-
-	/* char debug[255] = {'\0'}; */
-
-
-	printf("Original buffer:\n");
-	buffer_print(b);
-	return 0;
 
 	char *replace = "cccc";
 	buffer_replace_data_at_offset(b, 8, replace, 4);
 	printf("\nAfter replace:\n");
 	buffer_print(b);
 
-	return 0;
-	/* buffer_insert_at_offset(b, 23, "GGGG", 4); */
-	/* printf("\nAfter data inserting:\n"); */
-	/* buffer_print(b); */
+	buffer_insert_at_offset(b, 23, "GGGG", 4);
+	buffer_print(b);
 
 	buffer_delete_data_at_offset(b, 8, 4);
 	printf("\nAfter data deleting:\n");
 	buffer_print(b);
-	/* buffer_delete_data_at_offset(b, 0, 3); */
-	/* buffer_print(b); */
-
-	/* buffer_foreach_chunk(b, 0, (void (*)(Chunk_t*, void*))chunk_print); */
 
 	buffer_free(b);
 	return 0;
